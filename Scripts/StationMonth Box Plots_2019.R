178459a735e400354a9f2c2b5505d96afb77caf1
Jeff Good <goodj@g.cofc.ecu>
2021-02-17 23:23:31 -0500

    Edited Code that Works

--- Scripts/StationMonth Box Plots_2019.R
+++ Scripts/StationMonth Box Plots_2019.R
@@ -1,9 +1,11 @@
-#Script for Station Box Plots over year:
+#Script for Station Box Plots over year, 2019:
 
 squid <- read.csv("./Data/Lbrevis QM Data.csv", header = TRUE)
 abiotic <- read.csv("./Data/Lbrevis Abiotic Data.csv", header = TRUE)
 install.packages("tidyverse")
 install.packages("purrr")
+install.packages("reshape")
+library(reshape)
 library(purrr)
 library(tidyverse)
 library(dplyr)
@@ -23,53 +25,47 @@ view(squid)
 #Filtering for data for each site
 squid.Anc <- filter(squid, Station == "Anchorage")
 view(squid.Anc)
-view(squid.Anch)
 squid.Ftj = filter(squid, Station == "Fort Johnson")
+view(squid.Ftj)
 squid.LA = filter(squid, Station == "Lower Ashley")
 squid.UA = filter(squid, Station == "Upper Ashley")
 
 #Binding each subset with Month names
-squid.Anc$Month.name = factor(squid.Anc$Month.name, levels = c("Jan", "Feb",
-  "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))
-squid.Ftj$Month.name = factor(squid.Ftj$Month.name, levels = c("Jan", "Feb", 
-  "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))
-squid.LA$Month.name = factor(squid.LA$Month.name, levels = c("Jan", "Feb", 
-  "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))
-squid.UA$Month.name = factor(squid.UA$Month.name, levels = c("Jan", "Feb", 
-  "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))
+squid$Month.name = factor(squid$Month.name, levels = Month.name)
+squid.Anc$Month.name = factor(squid.Anc$Month.name, levels = Month.name)
+View(squid.Anc)
+squid.Ftj$Month.name = factor(squid.Ftj$Month.name, levels = Month.name)
+view(squid.Ftj)
+squid.LA$Month.name = factor(squid.LA$Month.name, levels = Month.name)
+squid.UA$Month.name = factor(squid.UA$Month.name, levels = Month.name)
 
 #Seperate Box Plots for each Station
 
-#Doesn't work: 
 ggplot(aes(x = Month.name, y = Gladius.Length, fill=Month.name), data = squid.Anc)+
-  geom_boxplot(show.legend=F, fill = "blue") + labs(title="Anchorage", x="Month", y = "Gladius Length (cm)") 
-#All, november and december months are missing. 
-#######################GUS############################
-##works: 
-ggplot(aes(x = Month.name, y = Gladius.Length, fill=Month.name), data = squid.Anch)+
-  geom_boxplot(show.legend=F, fill = "blue") + labs(title="Anchorage", x="Month", y = "Gladius Length (cm)")
+  geom_boxplot(show.legend=F, fill = "blue1") + labs(title="Anchorage", x="Month", y = "Gladius Length (cm)") 
 
+ggplot(aes(x = Month.name, y = Gladius.Length, fill=Month.name), data = squid.Ftj)+
+  geom_boxplot(show.legend=F, fill = "blue2") + labs(title="Fort Johnson", x="Month", y = "Gladius Length (cm)")
 
+ggplot(aes(x = Month.name, y = Gladius.Length, fill=Month.name), data = squid.LA)+
+  geom_boxplot(show.legend=F, fill = "blue3") + labs(title="Lower Ashley", x="Month", y = "Gladius Length (cm)")
 
+ggplot(aes(x = Month.name, y = Gladius.Length, fill=Month.name), data = squid.UA)+
+  geom_boxplot(show.legend=F, fill = "blue4") + labs(title="Upper Ashley", x="Month", y = "Gladius Length (cm)")
 
-LenbyMoStTest <- melt(LenbyMoSt, id.var = "Label")
-
-ggplot(aes(x = Month, y = Gladius.Length), data = Smith)+
-  geom_boxplot()+#default ggplot will use count
-  #need to specify x and y using stat = "identity"
-  facet_wrap(~Station, ncol=1)
-
-boxplot(Gladius.Length ~ Month , data = LenbyMoSt)
-
-boxplot(Gladius.Length ~ Month,
-        data = LenbyMoSt,
-        main = "Gladius Lengths per Month",
-        xlab = "Month Number",
-        ylab = "Average Gladius Length",
-        col = "blue",
-        border = "black")
-
-
-
-
+#All station summaries in one figure. 
+ggplot(aes(x = Month.name, y = Gladius.Length, fill=Month.name), data = squid)+
+  geom_boxplot(show.legend=F, fill = "blue") + 
+  labs(x="Month", y = "Gladius Length (cm)") + 
+  facet_wrap(~Station)
 ################################################################################
+
+#Box plot of gladius length distribution at each station overall in 2019
+boxplot(Gladius.Length ~ Station , data = squid)
+ggplot(aes(x=Station, y = Gladius.Length, fill = Station), data = squid) + 
+  geom_boxplot(show.legend=F) + labs(x="Station", y = "Gladius Length (cm)") +
+  scale_fill_brewer(palette = 3)
+
+
+
+
